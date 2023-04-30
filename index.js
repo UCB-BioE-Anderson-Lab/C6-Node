a0f5152e257a0685479d16a3498fae26a3135876
J. Christopher Anderson <jcanderson@berkeley.edu>
2023-04-29 23:22:03 -0700

    timestamp

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes'); // Import the routes
+const path = require('path'); // Import the path module
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use(routes); // Use the routes
+
+// Add a route to serve the openapi.yaml file
+app.get('/openapi.yaml', (req, res) => {
+  res.sendFile(path.join(__dirname, 'openapi.yaml'));
+});
+
+// Add a route to serve the manifest file (.well-known/ai-plugin.json)
+app.get('/.well-known/ai-plugin.json', (req, res) => {
+  res.sendFile(path.join(__dirname, '.well-known', 'ai-plugin.json'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- routes.js
+++ routes.js
@@ -2,110 +2,307 @@ const vm = require('vm');
 const express = require('express');
 const router = express.Router();
 const Datum = require('./models/Datum');
-const Function = require('./models/Function');
-const Schema = require('./models/Schema');
+const Function = require(
+    './models/Function');
+const Schema = require(
+    './models/Schema');
 
 // In-memory database for storing Sharable objects
 const db = {};
 
 // Read operation
-router.get('/api/read/:id', (req, res) => {
-  const id = req.params.id;
-  const sharable = db[id];
-  if (!sharable) {
-    return res.status(404).json({ error: 'ID not found' });
-  }
-  res.json(sharable);
-});
+router.get('/api/read/:id', (req,
+    res) => {
+        const id = req.params.id;
+        const sharable = db[id];
+        if (!sharable) {
+            return res.status(404)
+                .json({
+                    error: 'ID not found'
+                });
+        }
+        res.json(sharable);
+    });
+
+// Example JSON objects for the three different types
+const exampleFunction = {
+    id: 'example_function_id',
+    type: 'function',
+    code: '/* function code */'
+};
+
+const exampleDatum = {
+    id: 'example_datum_id',
+    type: 'datum',
+    schema: 'example_schema',
+    data: {
+        /* data fields */ }
+};
+
+const exampleSchema = {
+    id: 'example_schema_id',
+    type: 'schema',
+    fields: {
+        /* schema fields */ }
+};
+
+// Custom error class for validation errors
+class ValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name =
+            'ValidationError';
+    }
+}
 
 // Helper function to validate input data
 function validate(sharable, db) {
-  if (!sharable || !sharable.id || !sharable.type) {
-    return 'Invalid input or missing ID or type';
-  }
+    // Check if the 'type' field is defined
+    if (!sharable || !sharable.id || !
+        sharable.type) {
+        throw new ValidationError(
+            'Invalid input or missing ID or type. You have three choices for the type: function, datum, schema.\n' +
+            'Examples:\n' +
+            JSON.stringify(
+                exampleFunction,
+                null, 2) + '\n' +
+            JSON.stringify(
+                exampleDatum, null,
+                2) + '\n' +
+            JSON.stringify(
+                exampleSchema, null,
+                2)
+        );
+    }
 
-  if (sharable.type === 'datum') {
-    if (!sharable.schema) {
-      return 'Missing schema for datum';
+    // Check if the 'type' field has a valid value
+    if (!['function', 'datum', 'schema']
+        .includes(sharable.type)) {
+        throw new ValidationError(
+            `Invalid type '${sharable.type}'. You have three choices for the type: function, datum, schema.\n` +
+            'Examples:\n' +
+            JSON.stringify(
+                exampleFunction,
+                null, 2) + '\n' +
+            JSON.stringify(
+                exampleDatum, null,
+                2) + '\n' +
+            JSON.stringify(
+                exampleSchema, null,
+                2)
+        );
     }
-    const schema = db[sharable.schema];
-    if (!schema || schema.type !== 'schema') {
-      return 'Schema not found or invalid';
-    }
-    for (const field in sharable) {
-      if (field !== 'id' && field !== 'type' && field !== 'schema' && !schema.fields[field]) {
-        return `Invalid field '${field}' not defined in schema`;
-      }
-    }
-  }
 
-  return true;
+    // Validate based on the type
+    if (sharable.type === 'datum') {
+        if (!sharable.schema) {
+            throw new ValidationError(
+                'Missing schema for datum.\n' +
+                'Example:\n' +
+                JSON.stringify(
+                    exampleDatum,
+                    null, 2)
+            );
+        }
+        const schema = db[sharable
+            .schema];
+        if (!schema || schema.type !==
+            'schema') {
+            throw new ValidationError(
+                'Schema not found or invalid.\n' +
+                'Example:\n' +
+                JSON.stringify(
+                    exampleSchema,
+                    null, 2)
+            );
+        }
+        // Additional validation for datum fields based on the schema
+        if (!sharable.data ||
+            typeof sharable.data !==
+            'object') {
+            throw new ValidationError(
+                'Missing or invalid data field for datum.\n' +
+                'Example:\n' +
+                JSON.stringify(
+                    exampleDatum,
+                    null, 2)
+            );
+        }
+        for (const field in sharable
+                .data) {
+            if (!schema.fields[field]) {
+                throw new ValidationError(
+                    `Invalid field '${field}' not defined in schema.\n` +
+                    'Example:\n' +
+                    JSON.stringify(
+                        exampleDatum,
+                        null, 2)
+                );
+            }
+        }
+    } else if (sharable.type ===
+        'function') {
+        // Validation for function type
+        if (!sharable.code ||
+            typeof sharable.code !==
+            'string') {
+            throw new ValidationError(
+                'Missing or invalid code field for function.\n' +
+                'Example:\n' +
+                JSON.stringify(
+                    exampleFunction,
+                    null, 2)
+            );
+        }
+    } else if (sharable.type ===
+        'schema') {
+        // Validation for schema type
+        if (!sharable.fields ||
+            typeof sharable.fields !==
+            'object') {
+            throw new ValidationError(
+                'Missing or invalid fields for schema.\n' +
+                'Example:\n' +
+                JSON.stringify(
+                    exampleSchema,
+                    null, 2)
+            );
+        }
+    }
+
+    return true;
 }
 
 // Create operation
-router.post('/api/create', (req, res) => {
-  const sharable = req.body;
-  const validationResult = validate(sharable, db);
-  if (validationResult !== true) {
-    return res.status(400).json({ error: validationResult });
-  }
-  if (db[sharable.id]) {
-    return res.status(400).json({ error: 'ID already exists' });
-  }
-  db[sharable.id] = sharable;
-  res.json({ id: sharable.id });
+router.post('/api/create', (req,
+res) => {
+    const sharable = req.body;
+    const validationResult =
+        validate(sharable, db);
+    if (validationResult !==
+        true) {
+        return res.status(400)
+            .json({
+                error: validationResult
+            });
+    }
+    if (db[sharable.id]) {
+        return res.status(400)
+            .json({
+                error: 'ID already exists'
+            });
+    }
+    db[sharable.id] = sharable;
+    res.json({
+        id: sharable.id
+    });
 });
 
 // Update operation
-router.put('/api/update/:id', (req, res) => {
-  const id = req.params.id;
-  const sharable = req.body;
-  const validationResult = validate(sharable, db);
-  if (validationResult !== true) {
-    return res.status(400).json({ error: validationResult });
-  }
-  if (!sharable.id || sharable.id !== id) {
-    return res.status(400).json({ error: 'Invalid input or ID mismatch' });
-  }
-  db[id] = sharable;
-  res.json(sharable);
-});
+router.put('/api/update/:id', (req,
+    res) => {
+        const id = req.params.id;
+        const {
+            type,
+            data
+        } = req.body;
+
+        // Check if the sharable object exists in the database
+        if (!db[id]) {
+            return res.status(404)
+                .json({
+                    error: 'ID not found'
+                });
+        }
+
+        // Create a new sharable object with the updated properties
+        const updatedSharable = {
+            id: id,
+            type: type,
+            data: data
+        };
+
+        // Validate the updated sharable object
+        const validationResult =
+            validate(
+                updatedSharable, db
+                );
+        if (validationResult !==
+            true) {
+            return res.status(400)
+                .json({
+                    error: validationResult
+                });
+        }
+
+        // Perform the update operation
+        db[id] = updatedSharable;
+        res.json(updatedSharable);
+    });
 
 // Delete operation
-router.delete('/api/delete/:id', (req, res) => {
-  const id = req.params.id;
-  if (!db[id]) {
-    return res.status(404).json({ error: 'ID not found' });
-  }
-  delete db[id];
-  res.json({ success: true });
+router.delete('/api/delete/:id', (req,
+    res) => {
+    const id = req.params.id;
+    if (!db[id]) {
+        return res.status(404)
+            .json({
+                error: 'ID not found'
+            });
+    }
+    delete db[id];
+    res.json({
+        success: true
+    });
 });
 
 // Run operation for Functions
-router.post('/api/run/:id', (req, res) => {
-  console.log('Received request to run function');
-  const id = req.params.id;
-  const args = req.body.args;
-  console.log('Args:', args);
-  const sharable = db[id];
-  if (!sharable || sharable.type !== 'function') {
-    return res.status(404).json({ error: 'Function not found' });
-  }
-  console.log('Function found:', sharable);
-  const script = new vm.Script(sharable.code);
-  const context = vm.createContext();
-  script.runInContext(context);
-  let result;
-  try {
-    // Use the correct function name "revcomp" when calling the function
-    result = context.revcomp(...args);
-  } catch (error) {
-    console.error('Execution error:', error);
-    return res.status(500).json({ error: 'Execution error' });
-  }
-  console.log('Execution result:', result);
-  res.json({ result });
-});
+router.post('/api/run/:id', (req,
+    res) => {
+        console.log(
+            'Received request to run function'
+            );
+        const id = req.params.id;
+        const args = req.body.args;
+        console.log('Args:', args);
+        const sharable = db[id];
+        if (!sharable || sharable
+            .type !== 'function') {
+            return res.status(404)
+                .json({
+                    error: 'Function not found'
+                });
+        }
+        console.log(
+            'Function found:',
+            sharable);
+        const script = new vm
+            .Script(sharable.code);
+        const context = vm
+            .createContext();
+        script.runInContext(
+        context);
+        let result;
+        try {
+            // Use the correct function name "revcomp" when calling the function
+            result = context
+                .revcomp(...args);
+        } catch (error) {
+            console.error(
+                'Execution error:',
+                error);
+            return res.status(500)
+                .json({
+                    error: 'Execution error'
+                });
+        }
+        console.log(
+            'Execution result:',
+            result);
+        res.json({
+            result
+        });
+    });
 
 
 module.exports = router;
